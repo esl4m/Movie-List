34c2b3737d9434cc4b9e8ea1d0f13184bfa454a7
esl4m <me@esl4m.com>
2020-11-23 22:40:45 +0300

    Adds test cases for List Movies and List People

--- movies_app/tests.py
+++ movies_app/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class ListMoviesTest(TestCase):
+    """
+    Test module for List Movies view
+    """
+    def setUp(self):
+        self.base_url = 'http://0.0.0.0:8000/movies/'
+        self.client = Client()
+
+    def test_list_movies(self):
+        """ Test list movies view """
+        response = self.client.get(self.base_url)
+        # Test success listing
+        self.assertEqual(response.status_code, 200)
+        # Test empty object in html
+        # self.assertContains(response,"No movies available.")
+
+
+class ListPeopleTest(TestCase):
+    """
+    Test module for List People view
+    """
+    def setUp(self):
+        self.base_url = 'http://0.0.0.0:8000/movies/people/'
+        self.client = Client()
+    
+    def test_list_people(self):
+        """ Test list people view """
+        response = self.client.get(self.base_url)
+        # Test success listing
+        self.assertEqual(response.status_code, 200)
+        # Test empty object in html
+        # self.assertContains(response,"No people available.")
